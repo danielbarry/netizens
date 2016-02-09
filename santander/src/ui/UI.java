f07cc3fe3fa14bf9922c05a92ad272f4a076dd39
Dan <danbarry16@googlemail.com>
2016-02-09 20:46:38 +0000

    Stand in UI class

---
+++ santander/src/ui/UI.java
@@ -0,0 +1,18 @@
+package netizens.bank.util.ui;
+
+/**
+ * UI.java
+ *
+ * This class is responsible for handling the User Interface in its entirety.
+ **/
+public class UI{
+  /**
+   * UI()
+   *
+   * The constructor for the UI class, responsible for initialising all values
+   * required and starting the UI.
+   **/
+  public UI(){
+    /* TODO: Start the UI. */
+  }
+}
