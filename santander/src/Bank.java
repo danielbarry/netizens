093c02c1b4c019f52b7e4ea3baadf50248d6d278
Dan <danbarry16@googlemail.com>
2016-02-09 17:09:20 +0000

    Layout for the bank class

---
+++ santander/src/Bank.java
@@ -0,0 +1,24 @@
+package netizens.bank;
+
+/**
+ * Bank.java
+ *
+ * This class is responsible for initialising the various sections, depending
+ * on whether parameters are being parsed to the front-end or back-end.
+ **/
+public class Bank{
+  /**
+   * Bank()
+   *
+   * Here we look at distributing the various tasks to where they belong and
+   * handling immediate requests.
+   *
+   * @param args The arguments give to the program.
+   **/
+  public Bank(String[] args){
+    /* TODO: Firstly, parse all arguments and draw all the data from them. */
+    /* TODO: Decide if any immediate action is required, i.e. bad paramter,
+     *       help, version, etc. */
+    /* TODO: Now we must work out who to send the options to. */
+  }
+}
