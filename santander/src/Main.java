159e816ca8b1d36b6e07d468e1186951574c41d4
Dan <danbarry16@googlemail.com>
2016-02-09 16:48:24 +0000

    Catch all death case but kill -9

--- santander/src/Main.java
+++ santander/src/Main.java
@@ -7,6 +7,10 @@ package netizens.bank;
  * parameters to the program.
  **/
 public class Main{
+  public enum EXIT_STATUS{
+    UNKNOWN, PLANNED, UNPLANNED, ERROR;
+  }
+
   /**
    * main()
    *
@@ -14,8 +18,43 @@ public class Main{
    *
    * @param args The arguments accepted from the command line.
    **/
-  public static void main(String[] args){
-    /* TODO: Write code here. */
-    System.out.println("Test");
+  public static void main(String[] args) throws InterruptedException{
+    Runtime.getRuntime().addShutdownHook(
+      new Thread(){
+        @Override
+        public void run(){
+          exit(EXIT_STATUS.UNPLANNED);
+        }
+      }
+    );
+  }
+
+  /**
+   * exit()
+   *
+   * Exits the program appropriately depending on how the program was asked to
+   * shutdown.
+   *
+   * @param status This lets the exit code know how long it has to shutdown. If
+   * the shutdown doesn't have much time, only core tasks should be done before
+   * shutting down.
+   **/
+  public static void exit(EXIT_STATUS status){
+    switch(status){
+      case PLANNED :
+        /* TODO: Shutdown resources as safely as possible, we have all the time
+         *       in the world. */
+        break;
+      case ERROR :
+        /* TODO: Shutdown resources safely, but bare in mind that they may not
+         *       respond correctly due to the error that occurred. After that,
+         *       print error and flush all messages. */
+        break;
+      case UNKNOWN :
+      case UNPLANNED :
+      default :
+        /* TODO: Close all important resources as fast as possible. */
+        break;
+    }
   }
 }
