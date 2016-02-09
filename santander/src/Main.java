5245ab29e893ae424dcfc6b5209c2ec9b1d4007e
Dan <danbarry16@googlemail.com>
2016-02-09 16:33:35 +0000

    Add ant building for Santander project

---
+++ santander/src/Main.java
@@ -0,0 +1,21 @@
+package netizens.bank;
+
+/**
+ * Main.java
+ *
+ * Entry class into the program, responsible for dealing with any input
+ * parameters to the program.
+ **/
+public class Main{
+  /**
+   * main()
+   *
+   * Entry point into the Java program.
+   *
+   * @param args The arguments accepted from the command line.
+   **/
+  public static void main(String[] args){
+    /* TODO: Write code here. */
+    System.out.println("Test");
+  }
+}
