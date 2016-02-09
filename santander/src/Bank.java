7d93cbf9318c2b3e46b39b2ab548bbe9bd14ad1c
Dan <danbarry16@googlemail.com>
2016-02-09 20:46:58 +0000

    Parameter parsing layout

--- santander/src/Bank.java
+++ santander/src/Bank.java
@@ -1,5 +1,6 @@
 package netizens.bank;
 
+import netizens.bank.Main;
 import netizens.bank.utils.Debug;
 
 /**
@@ -9,6 +10,10 @@ import netizens.bank.utils.Debug;
  * on whether parameters are being parsed to the front-end or back-end.
  **/
 public class Bank{
+  public enum MODE{
+    NONE, ERROR, HELP, VERSION, UI, SERVER
+  }
+
   /**
    * Bank()
    *
@@ -18,10 +23,95 @@ public class Bank{
    * @param args The arguments give to the program.
    **/
   public Bank(String[] args){
-    /* TODO: Firstly, parse all arguments and draw all the data from them. */
-    /* TODO: Decide if any immediate action is required, i.e. bad paramter,
-     *       help, version, etc. */
-    /* TODO: Now we must work out who to send the options to. */
+    /* Store information that may be helpful later */
+    MODE mode = MODE.NONE;
+    /* Firstly, parse all arguments and draw all the data from them */
+    for(int x = 0; x < args.length; x++){
+      /* Is the string large enough to check? */
+      if(args[x].length() > 0){
+        /* Is this an argument? */
+        if(args[x].charAt(0) == '-'){
+          /* Is the next section large enough to check? */
+          if(args[x].length() > 1){
+            /* Is this a small argument or full argument? */
+            if(args[x].charAt(1) == '-'){
+              /* Convert the values */
+              switch(args[x].substring(2)){
+                case "help" :
+                  args[x] = "-h";
+                  break;
+                case "server" :
+                  args[x] = "-s";
+                  break;
+                case "ui" :
+                  args[x] = "-u";
+                  break;
+                case "version" :
+                  args[x] = "-v";
+                  break;
+              }
+            }
+            /* Check the values */
+            switch(args[x].charAt(1)){
+              case 'h' :
+                /* Override any mode other than error */
+                if(mode != MODE.ERROR){
+                  mode = MODE.HELP;
+                }
+                break;
+              case 's' :
+                /* Only override if no other mode set */
+                if(mode == MODE.NONE){
+                  mode = MODE.SERVER;
+                }
+                break;
+              case 'u' :
+                /* Only override if no other mode set */
+                if(mode == MODE.NONE){
+                  mode = MODE.UI;
+                }
+                break;
+              case 'v' :
+                /* Override any mode other than error */
+                if(mode != MODE.VERSION){
+                  mode = MODE.VERSION;
+                }
+                break;
+              default :
+                /* If there has been an issue, go into error mode */
+                mode = MODE.ERROR;
+                break;
+            }
+
+          }
+        }
+      }
+    }
+    /* Complete actions for final mode */
+    switch(mode){
+      case HELP :
+        help();
+        Main.exit(Main.EXIT_STATUS.PLANNED);
+        break;
+      case SERVER :
+        /* TODO: Write this section. */
+        break;
+      case UI :
+        /* TODO: Write this section. */
+        break;
+      case VERSION :
+        version();
+        Main.exit(Main.EXIT_STATUS.PLANNED);
+        break;
+      case NONE :
+        System.err.println("Program was not asked to perform a task.");
+        break;
+      case ERROR :
+        System.err.println("Error in parameter.");
+      default :
+        Main.exit(Main.EXIT_STATUS.ERROR);
+        break;
+    }
     Debug.println("Parse arguments");
   }
 
@@ -33,5 +123,47 @@ public class Bank{
   public void run(){
     /* TODO: Run the modules we found in the command line parameters. */
     Debug.println("Run modules");
+  }
+
+  /**
+   * help()
+   *
+   * Displays the help for the program.
+   **/
+  private void help(){
+    System.out.println(
+      "\nPROGRAM <MODE> [OPT]" +
+      "\n" +
+      "\n  MODE" +
+      "\n" +
+      "\n    --help      -h" +
+      "\n      Displays this help." +
+      "\n" +
+      "\n    --server    -s" +
+      "\n      OPTions" +
+      "\n        TODO: To be written." +
+      "\n" +
+      "\n    --ui        -u" +
+      "\n      OPTions" +
+      "\n        TODO: To be written." +
+      "\n    --version   -v" +
+      "\n      Displays program version." +
+      "\n" +
+      "\n  Examples" +
+      "\n" +
+      "\n    TODO: Write examples."
+    );
+  }
+
+  /**
+   * version()
+   *
+   * Displays the version of the program.
+   **/
+  private void version(){
+    /* TODO: Get the version from JSON. */
+    System.out.println(
+      "Version 0.0.0"
+    );
   }
 }
