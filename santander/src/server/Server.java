62f4c65e7e7febc2f3e011928d933a42b51b871c
Dan <danbarry16@googlemail.com>
2016-03-01 01:55:00 +0000

    Simple Server class

---
+++ santander/src/server/Server.java
@@ -0,0 +1,22 @@
+package netizens.bank.server;
+
+import org.json.JSONTokener;
+
+/**
+ * Server.java
+ *
+ * This class handles the simple server input and output.
+ **/
+public class Server{
+  /**
+   * Server()
+   *
+   * The main server initialisation file.
+   *
+   * @param mainJSON The main configuration file containing the settings for
+   * the server.
+   **/
+  public Server(JSONTokener mainJSON){
+    /* TODO: Write this method. */
+  }
+}
