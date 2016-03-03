058b80f8bab0946214938a96335f62d6528f23f2
Dan <danbarry16@googlemail.com>
2016-03-03 23:10:00 +0000

    Basic fingerprint reader code

---
+++ santander/src/hardware/finger.cc
@@ -0,0 +1,26 @@
+/**
+ * finger.cc
+ *
+ * This class handles obtaining a hash of a fingerprint and displaying this
+ * over the terminal. No other information should be printed to the terminal so
+ * the program output may be wrapped in normal operation.
+ *
+ * Program written by B[]
+ **/
+
+/* NOTE: Below is required for libfprint to compile. */
+#include <cstddef>
+/* NOTE: This is found at /usr/include/libfprint/fprint.h. */
+#include "libfprint/fprint.h"
+
+/**
+ * main()
+ *
+ * The entry point of the program.
+ *
+ * @param argc The number of parameters.
+ * @param argv The parameters passed to the program.
+ **/
+int main(int argc, char** argv){
+  /* TODO: Write this code. */
+}
