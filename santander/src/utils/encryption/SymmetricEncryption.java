f99292d2d5b33b27c72663b234e535bec094bfa2
Robert Northard <robertnorthard@googlemail.com>
2016-03-06 21:51:14 +0000

    Formatted class - wrong indentation settings
    Signed-off-by: Robert Northard <robertnorthard@googlemail.com>

--- santander/src/utils/encryption/SymmetricEncryption.java
+++ santander/src/utils/encryption/SymmetricEncryption.java
@@ -1,4 +1,3 @@
-
 package netizens.bank.utils.encryption;
 
 import java.util.Set;
@@ -18,116 +17,114 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
-* Symmetric key encryption utility class.
-* 
+ * Symmetric key encryption utility class.
+ * 
 * @author robertnorthard
-*/
-public class SymmetricEncryption{
+ */
+public class SymmetricEncryption {
 
-	// class logger
-	private static final Logger LOGGER = Logger.getLogger(
-		SymmetricEncryption.class.getName());
+    // class logger
+    private static final Logger LOGGER = Logger.getLogger(
+            SymmetricEncryption.class.getName());
 
-	private static final String HASH_ALGORITHM = "SHA-256";
-	private static final String SYMMETRIC_ALGORITHM = "AES";
-	private static final String CHARACTER_ENCODING = "UTF-8";
+    private static final String HASH_ALGORITHM = "SHA-256";
+    private static final String SYMMETRIC_ALGORITHM = "AES";
+    private static final String CHARACTER_ENCODING = "UTF-8";
 
-	/**
-	* Create composite symmetric encryption key from set of credentials.
-	*
-	* @param credentials.
-	* @return generate symmetric key, null if error.
-	* @throws IllegalArgumentException 
-	*		if credentials is null or size is less than or equal 0.
-	*/
-	public static Key generateKey(Set<String> credentials){
+    /**
+     * Create composite symmetric encryption key from set of credentials.
+     *
+     * @param credentials.
+     * @return generate symmetric key, null if error.
+     * @throws IllegalArgumentException if credentials is null or size is less
+     * than or equal 0.
+     */
+    public static Key generateKey(Set<String> credentials) {
 
-		if(credentials == null || credentials.size() <= 0){
-			throw new IllegalArgumentException("Credentials cannot be null and must contain at least one type of biometric.");
-		}
+        if (credentials == null || credentials.size() <= 0) {
+            throw new IllegalArgumentException("Credentials cannot be null and must contain at least one type of biometric.");
+        }
 
-		Key symmetricKey = null;
+        Key symmetricKey = null;
 
-		StringBuffer key = new StringBuffer();
+        StringBuffer key = new StringBuffer();
 
-		for(String k: credentials){
-			key.append(k);
-		}
+        for (String k : credentials) {
+            key.append(k);
+        }
 
-		try{
-			MessageDigest md = MessageDigest.getInstance(
-				SymmetricEncryption.HASH_ALGORITHM);
+        try {
+            MessageDigest md = MessageDigest.getInstance(
+                    SymmetricEncryption.HASH_ALGORITHM);
 
-			md.update(
-				key.toString().getBytes(
-					Charset.forName(
-						SymmetricEncryption.CHARACTER_ENCODING)));
+            md.update(
+                    key.toString().getBytes(
+                            Charset.forName(
+                                    SymmetricEncryption.CHARACTER_ENCODING)));
 
-			 byte[] compositeDigest = md.digest();
+            byte[] compositeDigest = md.digest();
 
-			symmetricKey = new SecretKeySpec(compositeDigest, SymmetricEncryption.SYMMETRIC_ALGORITHM);
+            symmetricKey = new SecretKeySpec(compositeDigest, SymmetricEncryption.SYMMETRIC_ALGORITHM);
 
-			return null;
+            return null;
 
-		}catch(NoSuchAlgorithmException ex){
-			LOGGER.log(Level.SEVERE, null, ex);
-			return null;
-		}
-	}
+        } catch (NoSuchAlgorithmException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 
-	/**
-	* Encrypt a message.
-	*
-	* @param message message to encrypt.
-	* @param key key to use for encryption.
-	* @return encrypted message, null if error.
-	*/
-	public static byte[] encrypt(byte[] message, Key key){
+    /**
+     * Encrypt a message.
+     *
+     * @param message message to encrypt.
+     * @param key key to use for encryption.
+     * @return encrypted message, null if error.
+     */
+    public static byte[] encrypt(byte[] message, Key key) {
 
-		try{
- 			Cipher c = Cipher.getInstance(SymmetricEncryption.SYMMETRIC_ALGORITHM);
- 			c.init(Cipher.ENCRYPT_MODE, key);
-            
-    	    return c.doFinal(message);
+        try {
+            Cipher c = Cipher.getInstance(SymmetricEncryption.SYMMETRIC_ALGORITHM);
+            c.init(Cipher.ENCRYPT_MODE, key);
 
-	    }catch(
-	    	NoSuchAlgorithmException | 
-	    	InvalidKeyException |
-	    	IllegalBlockSizeException |
-	    	BadPaddingException |
-	    	NoSuchPaddingException ex){
+            return c.doFinal(message);
 
-			LOGGER.log(Level.SEVERE, null, ex);
-			return null;
-		}
+        } catch (NoSuchAlgorithmException |
+                InvalidKeyException |
+                IllegalBlockSizeException |
+                BadPaddingException |
+                NoSuchPaddingException ex) {
 
-	}
+            LOGGER.log(Level.SEVERE, null, ex);
+            return null;
+        }
 
-	/**
-	* Decrypt a message.
-	*
-	* @param message message to decrypt.
-	* @param key key to use for decryption.
-	* @return decrypted message, null if error.
-	*/
-	public static byte[] decrypt(byte[] message, Key key){
+    }
 
-		try{
- 			Cipher c = Cipher.getInstance(SymmetricEncryption.SYMMETRIC_ALGORITHM);
- 			c.init(Cipher.DECRYPT_MODE, key);
-            
-    	    return c.doFinal(message);
+    /**
+     * Decrypt a message.
+     *
+     * @param message message to decrypt.
+     * @param key key to use for decryption.
+     * @return decrypted message, null if error
+     */
+    public static byte[] decrypt(byte[] message, Key key) {
 
-	    }catch(
-	    	NoSuchAlgorithmException | 
-	    	InvalidKeyException |
-	    	IllegalBlockSizeException |
-	    	BadPaddingException |
-	    	NoSuchPaddingException ex){
+        try {
+            Cipher c = Cipher.getInstance(SymmetricEncryption.SYMMETRIC_ALGORITHM);
+            c.init(Cipher.DECRYPT_MODE, key);
 
-			LOGGER.log(Level.SEVERE, null, ex);
-			return null;
-		}
+            return c.doFinal(message);
 
-	}
+        } catch (NoSuchAlgorithmException |
+                InvalidKeyException |
+                IllegalBlockSizeException |
+                BadPaddingException |
+                NoSuchPaddingException ex) {
+
+            LOGGER.log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
 }
