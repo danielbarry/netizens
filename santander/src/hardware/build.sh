366091306fff01c3bf0a4073b82476dcfb9a41d6
Dan <danbarry16@googlemail.com>
2016-03-08 23:19:24 +0000

    Print debug information on build script

--- santander/src/hardware/build.sh
+++ santander/src/hardware/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo ">>>> Removing old files..."
 rm *.bin
+echo ">>>> Old files removed..."
 
+echo ">>>> Building binary..."
 gcc main.c -l fprint -o finger.bin
+echo ">>>> Binary built..."
+
+echo ">>>> Changing binary permissions..."
 chmod +x finger.bin
+echo ">>>> Binary permissions changed..."
