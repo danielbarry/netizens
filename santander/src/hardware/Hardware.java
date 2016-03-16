95524e892313fe7524d9cd1114f47c3416b11192
Dan <danbarry16@googlemail.com>
2016-03-16 02:46:19 +0000

    Define hardware wrapper

--- santander/src/hardware/Hardware.java
+++ santander/src/hardware/Hardware.java
@@ -1,5 +1,24 @@
 package netizens.bank.hardware;
 
+import netizens.bank.hardware.IO_TYPE;
+
+/**
+ * Hardware.java
+ *
+ * This class is responsible for generating hashes very hardware of given
+ * types.
+ **/
 public class Hardware{
-  /* TODO: Build this class. */
+  /**
+   * getHash()
+   *
+   * Gets the hash for a given type of hardware attached.
+   *
+   * @param type The type of the hardware to be read.
+   * @return The String value to be hashed.
+   **/
+  public String getHash(IO_TYPE type){
+    /* TODO: Write code. */
+    return null;
+  }
 }
